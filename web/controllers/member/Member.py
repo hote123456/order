2fa51c5060355e30e20db42af002ced8a714424b
magic <82634593@qq.com>
2018-10-18 13:40:27 +0800

    Chapter 9 completed

--- web/controllers/member/Member.py
+++ web/controllers/member/Member.py
@@ -2,21 +2,24 @@
 from flask import Blueprint
 from common.libs.Helper import ops_render
 
-route_member = Blueprint( 'member_page',__name__ )
+route_member = Blueprint('member_page', __name__)
 
-@route_member.route( "/index" )
+
+@route_member.route("/index")
 def index():
-    return ops_render( "member/index.html" )
+    return ops_render("member/index.html")
 
-@route_member.route( "/info" )
+
+@route_member.route("/info")
 def info():
-    return ops_render( "member/info.html" )
+    return ops_render("member/info.html")
 
-@route_member.route( "/set" )
+
+@route_member.route("/set")
 def set():
-    return ops_render( "member/set.html" )
+    return ops_render("member/set.html")
 
 
-@route_member.route( "/comment" )
+@route_member.route("/comment")
 def comment():
-    return ops_render( "member/comment.html" )
+    return ops_render("member/comment.html")
